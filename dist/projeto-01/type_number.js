7fed9d38dfdcfc9a460d92ae6d498fde7160be98
Romulo Rosa | Front-End <romulorosafull@gmail.com>
2025-10-10 14:45:22 -0300

    type number

---
+++ dist/projeto-01/type_number.js
@@ -0,0 +1,6 @@
+"use strict";
+let number1 = 10;
+let number2;
+number2 = 20;
+let sum = number1 + number2;
+console.log(sum);
---
+++ src/projeto-01/type_number.ts
@@ -0,0 +1,8 @@
+let number1: number = 10;
+
+let number2: number;
+number2 = 20;
+
+let sum = number1 + number2;
+
+console.log(sum);
