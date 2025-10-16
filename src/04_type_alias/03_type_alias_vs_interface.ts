39c6cf1cdbb7ae17d1c4acf6f412d75c76a2547c
Romulo Rosa | Front-End <romulorosafull@gmail.com>
2025-10-16 14:20:44 -0300

    type alias vs interface

---
+++ src/04_type_alias/03_type_alias_vs_interface.ts
@@ -0,0 +1,33 @@
+// TYPE ALIAS vs INTERFACES
+
+// PONTOS EM COMUM ENTRE ALIAS E INTERFACES
+// Ambos são utilizados para definir:
+// - Novos tipos de dados
+// - Regras de um tipo de dado
+// - Contrato/padrão de um tipo de dado
+// - Tipo de dados que pode ter vários valores, tipos de dados, métodos, construtores e herança
+
+
+
+// DIFERENÇAS ENTRE ALIAS E INTERFACES
+
+// ALIASES usar para :
+// - Simplificar o código e evitar repetições.
+// - Renomear tipos
+// - Renomear tipos sem restringir os tipos de dados.
+// - Restringir tipos de dados e construir contratos.
+// - Criar novos tipos a partir de outros tipos e adicionar novos métodos/propriedades, construtores e herança.
+
+// INTERFACES usar para:
+// - Estruturar o código, organizar e documentar os tipos de dados.
+// - Restringir os tipos de dados, construir contratos, regras e herança
+// - Criar novos tipos a partir de outros tipos, métodos/propriedades, novos construtores, herança,
+//   restrições de tipos, valores, métodos, construtores, herança e herança múltipla.
+
+
+// RECOMENDAÇÕES
+// - Num projeto em andamento seguir o que já estiver em uso
+// - Lembrar que num mesmo projeto não é necessário usar ambos, mas podem ser úteis para
+//   estruturar o código, organizar e documentar o tipo de dados
+
+
