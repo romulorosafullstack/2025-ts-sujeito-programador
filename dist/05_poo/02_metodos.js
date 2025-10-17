ecdb81ca57f37bac3c1fd8dcffe80c7cf29a1f93
Romulo Rosa | Front-End <romulorosafull@gmail.com>
2025-10-17 17:21:46 -0300

    poo metodos

--- dist/05_poo/02_metodos.js
+++ dist/05_poo/02_metodos.js
@@ -10,6 +10,20 @@ class Loja1 {
         console.log(`Nome: ${this.nome} | Categoria: ${this.categoria}`);
         console.log("-----------------------------------");
     }
+    emitirPedido(mesa, ...pedidos) {
+        return `Pedido da Mesa: ${mesa}\nItens: ${pedidos.join(", ")}`;
+    }
+    alterarStatus(novoStatus) {
+        if (novoStatus === "ativo") {
+            console.log("Status: ativo");
+        }
+        else {
+            console.log("Status: inativo");
+        }
+    }
 }
 const loja1a = new Loja1("Magazine Luiza", "E-commerce");
 loja1a.criarLoja();
+const retornoLoja = loja1a.emitirPedido(10, "Café", "Pizza", "Bebida");
+console.log(retornoLoja);
+loja1a.alterarStatus("inativo");
--- src/05_poo/02_metodos.ts
+++ src/05_poo/02_metodos.ts
@@ -3,6 +3,7 @@
 // São chamados usando o operador '.'
 // São chamados usando o operador '()'
 
+type Status = "ativo" | "inativo";
 
 // Crio a Class
 class Loja1 {
@@ -21,6 +22,20 @@ class Loja1 {
     console.log(`Nome: ${this.nome} | Categoria: ${this.categoria}`);
     console.log("-----------------------------------");
   }
+
+  // OUTRO MÉTODO passando parametro e rest params
+  emitirPedido(mesa: number, ...pedidos: string[]): string {
+    return `Pedido da Mesa: ${mesa}\nItens: ${pedidos.join(", ")}`;
+  }
+
+  // OUTRO MÉTODO usando type alias
+  alterarStatus(novoStatus: Status): void {
+    if (novoStatus === "ativo") {
+      console.log("Status: ativo");
+    } else {
+      console.log("Status: inativo");
+    }
+  }
 }
 
 // Instancio a classe Loja1 e passo valores aos seus atributos
@@ -28,6 +43,16 @@ const loja1a = new Loja1("Magazine Luiza", "E-commerce");
 
 // Chamo o método criarLoja() do objeto loja1a
 loja1a.criarLoja();
+
+// Chamo o método emitirPedido() do objeto loja1a
+const retornoLoja = loja1a.emitirPedido(10, "Café", "Pizza", "Bebida" );
+console.log(retornoLoja);
+
+// Chamo o método alterarStatus() do objeto loja1a
+// Como uso type alias para definir o novoStatus, só posso usar como valor "ativo" ou "inativo"
+loja1a.alterarStatus("inativo");
+
+
 
 
 
