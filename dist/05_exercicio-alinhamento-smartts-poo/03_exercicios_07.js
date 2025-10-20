de953d09dc4ab3ac563ec9d394663c6ce730bb93
Romulo Rosa | Front-End <romulorosafull@gmail.com>
2025-10-20 10:29:31 -0300

    poo 03 metodos conta corrente sacar e verSaldo

--- dist/05_exercicio-alinhamento-smartts-poo/03_exercicios_07.js
+++ dist/05_exercicio-alinhamento-smartts-poo/03_exercicios_07.js
@@ -8,8 +8,26 @@ class Conta {
     }
 }
 class ContaCorrente extends Conta {
+    cpf;
+    constructor(titular, saldoInicial, cpf) {
+        super(titular, saldoInicial);
+        this.cpf = cpf;
+    }
     depositar(valor) {
         this.saldo += valor;
         console.log(`Depositado ${valor} reais na conta de ${this.titular}`);
+    }
+    sacar(valor) {
+        console.log(`TITULAR: ${this.titular}`);
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            console.log(`SAQUE AUTORIZADO.\nVALOR: R$ ${valor}`);
+        }
+        else {
+            console.log(`SALDO INSUFICIENTE`);
+        }
+    }
+    verSaldo() {
+        console.log(`SALDO ATUAL DA CONTA: R$ ${this.saldo}`);
     }
 }
--- src/05_exercicio-alinhamento-smartts-poo/03_exercicios_07.ts
+++ src/05_exercicio-alinhamento-smartts-poo/03_exercicios_07.ts
@@ -10,6 +10,15 @@ class Conta{
 }
 
 class ContaCorrente extends Conta {
+  cpf: string;
+
+  // Crio o construtor da subclasse (ContaCorrente) passando os primeiro os params da clase pai/superclasse e depois da classe filha/subclasse
+  constructor(titular: string, saldoInicial: number, cpf: string) {
+    // Chamo o construtor da superclasse (Conta) passando os params
+    super(titular, saldoInicial);
+    this.cpf = cpf;
+  }
+
   // Adicionei método para depositar
   // Passo um param valor de tipo number
   // Uso void pois a função não tem return, pois só usa um console.log
@@ -18,5 +27,28 @@ class ContaCorrente extends Conta {
     this.saldo += valor;
     //Exibo o saldo atual
     console.log(`Depositado ${valor} reais na conta de ${this.titular}`);
+  }
+  
+  // Adicionei método para sacar
+  // Passo um param valor de tipo number
+  // Uso void pois a função não tem return, pois só usa um console.log
+  sacar(valor: number): void {
+    console.log(`TITULAR: ${this.titular}`);
+    // Verifico se o saldo é maior ou igual ao valor do saque
+    if (this.saldo >= valor) {
+      // Decremento o valor do saque do saldo atual
+      this.saldo -= valor;
+      // Exibo uma mensagem confirmando o saque
+      console.log(`SAQUE AUTORIZADO.\nVALOR: R$ ${valor}`);
+    } else {
+      // Exibo uma mensagem negando o saque por saldo insuficiente
+      console.log(`SALDO INSUFICIENTE`);
+    }
+  }
+
+  // Adicionei método verSaldo
+  // Uso void pois a função não tem return, pois só usa um console.log
+  verSaldo(): void {
+    console.log(`SALDO ATUAL DA CONTA: R$ ${this.saldo}`);
   }
 }
